f0b5e8c7dc29492a1ecb50925c55a184870c8be6
Marco Antonio Júnior Plá <pla@Marcos-MacBook-Air.local>
2020-01-24 18:38:36 -0300

    arrow function and spread operator added. Model, view and controller were separated

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -13,11 +13,19 @@ class NegociacaoController {
 
         event.preventDefault();
         
+        let data = new Date(
+            ...this._inputData.value
+                .split('-')
+                .map((item, indice) => item - indice % 2)       
+        );
+       console.log(typeof(data));
+       console.log(data);
         let negociacao = new Negociacao(
-            this._inputData.value,
+            data,
             this._inputQuantidade.value,
             this._inputValor.value
-        ); 
-
+        );
+        
+        console.log(negociacao);
     }
 }
