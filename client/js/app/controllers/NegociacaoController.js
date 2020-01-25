8e26f1e662a92deaf5ee76cb2f5f29865619e6e9
Marco Antonio Júnior Plá <pla@Marcos-MacBook-Air.local>
2020-01-25 17:35:07 -0300

    ListModel and NegociacaoController private methods

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,21 +6,34 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
 
     adiciona(event) {
 
         event.preventDefault();
-        
-        let helper = new DateHelper();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
 
-        let negociacao = new Negociacao(
-            helper.textoParaData(this._inputData.value),
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao (){
+
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
 
-        console.log(negociacao);
+    _limpaFormulario(){
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        
+        this._inputData.focus();
     }
 }
