3456be8884df3030b397c8c7859383043f80a028
Marco Plá <marco.pla@gmail.com>
2020-01-28 17:41:12 -0300

    Reflect.apply added function

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,13 +6,24 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(this, function(model){
+            
+            console.log(this);
+            this._negociacoesView.update(model);
+        });
         
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
+        this._negociacoesView.update(this._listaNegociacoes); // maté mesmo com a armadilha para fazer a primeira renderezacao da lista
 
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
+    }
+
+    apaga(){
+
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso.';
         this._mensagemView.update(this._mensagem);
     }
 
@@ -20,8 +31,6 @@ class NegociacaoController {
 
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
-        
         this._mensagem.texto = 'Negociação realizada com sucesso.';
         this._mensagemView.update(this._mensagem);
         
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,17 +1,27 @@
 class ListaNegociacoes{
 
-    constructor(){
+    constructor(contexto, armadilha){
         
         this._negociacoes = [];
+        this._armadilha = armadilha;
+        this._contexto = contexto;
     }
 
     adiciona(negociacao){
 
         this._negociacoes.push(negociacao);
+        //this._armadilha(contexto);
+        Reflect.apply(this._armadilha, this._contexto), [this];
     }
 
     get negociacoes() {
 
         return [].concat(this._negociacoes);
+    }
+
+    esvazia(_negociacoes){
+        
+        this._negociacoes = [];
+        this._armadilha(contexto);
     }
 }
