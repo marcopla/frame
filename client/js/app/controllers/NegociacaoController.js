8fe8d06d999e82304397787716e84000a0c46996
Marco Plá <marco.pla@gmail.com>
2020-02-07 15:49:15 -0300

    xhr added

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -23,6 +23,36 @@ class NegociacaoController {
 
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso.';
+        console.log(this._mensagem);
+    }
+
+    importaNegociacoes(){
+        
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/semana');
+        xhr.onreadystatechange = () => {
+
+            if(xhr.readyState == 4){
+
+                if(xhr.status == 200){
+                
+                    JSON.parse(xhr.responseText)
+                    .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                    .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    this._mensagem.texto = 'Negociações importadas com sucesso.';
+
+                }else{
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = 'Não foi possível obter as negociações.';  
+                }
+
+            }
+
+        };
+
+        xhr.send();
+
     }
 
     adiciona(event) {
